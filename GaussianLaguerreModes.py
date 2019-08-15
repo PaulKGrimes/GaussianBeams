4163a46c4b9e30c30e5edf4f2170e04b00b4abe6
Paul Grimes <pgrimes@cfa.harvard.edu>
2019-08-15 13:23:26 -0400

    Update GaussianLaguerreModes.py
    Add conversions between Gaussian beam parameters
    Implement direct calculation of full Gaussian beam mode and Gaussian beam mode amplitudes.

--- GaussianLaguerreModes.py
+++ GaussianLaguerreModes.py
@@ -15,82 +15,261 @@ from math import factorial
 
 j = 1j
 
-def k(self, f, e_r=1.0):
-    """return the wavenumber for a wave at frequency f in medium with dielectric constant e_r (default e_r = 1.0)"""
-    return 2*np.pi*f/c
+def Epl(rho, phi, k, w, R, p=0, l=0):
+    """Calculate complex value of normalized Gauss-Laguerre mode at rho, phi, z in
+    cylindrical coordinates
 
-# Everything below here copied from ModifiedGaussianLaguerreModes and needs altering
-def alpha(rho, ww, FF):
-    """return the alpha parameter for the modified Gauss-Laguerre modes"""
-    return np.sqrt(2)*rho/(ww*FF)
+    Arguments:
+        rho: radial distance - numpy array or float.
+        phi: azimuthal angle - numpy array or float.
+        k:   wavenumber of beam - float.
+        w:   beam radius - float.
+        R:   radius of phase curvature - float.
+        p:   axial mode index - integer.
+        l:   azimuthal mode index - integer
+    Returns:
+        complex value of field - numpy array of complex values"""
+    # Convert rho and phi into meshgrids
+    rho, phi = make_grids(rho, phi)
 
-def alpha_ff(theta, w0, kk):
-    """return the farfield alpha parameter for the modified Gauss-Laguerre modes"""
-    return 1./np.sqrt(2) * kk * w0 * np.sin(theta)
+    lm = 2*np.pi/k
+    z = z_from_wR(w, R, lm)
+    phi0 = phi0_from_w0z(w0, z, lm)
 
-def F(rho, z, bb):
-    r = R(z, bb)
-    return np.sqrt(1+(rho/r)**2)
+    phase_exponent = - j*k*z - j*np.pi*rho**2/(lm*R**2) + j*(2*p+l+1)*phi0
+    phi_exponent = j*l*phi
 
-def b(w0, kk):
-    """return the value of b"""
-    return kk*(w0**2)/2.0
+    amp = Epl_amp(rho, phi, k, w, R, p=0, l=0)
 
-def Phi0(z, bb):
-    return np.arctan(z/bb)
+    return amp*np.exp(phase_exponent)*np.exp(phi_exponent)
 
-def R(z, bb):
-    return z*(1+(bb/z)**2)
 
-def w(z, w0, bb):
-    return w0*np.sqrt(1+(z/bb)**2)
+def Epl_amp(rho, phi, k, w, R, p=0, l=0):
+    """Calculate the amplitude of normalized Gauss-Laguerre mode at rho, phi, z in
+    cylindrical coordinates
 
-def F(rho, z, bb):
-    r = R(z, bb)
-    return np.sqrt(1+(rho/r)**2)
+    Arguments:
+        rho: radial distance - numpy array or float.
+        phi: azimuthal angle - numpy array or float.
+        k:   wavenumber of beam - float.
+        w:   beam radius - float.
+        R:   radius of phase curvature - float.
+        p:   axial mode index - integer.
+        l:   azimuthal mode index - integer
+    Returns:
+        amplitude of the field - numpy array of float values"""
+    # Convert rho and phi into meshgrids
+    rho, phi = make_grids(rho, phi)
+
+    lm = 2*np.pi/k
+    z = z_from_wR(w, R, lm)
+    phi0 = phi0_from_w0z(w0, z, lm)
+
+    amp_exponent = -rho**2/w**2
+
+    Spl = Sr(rho, w, p, m)
+
+    nrm = norm(w, p, l)
+
+    return nrm*Spl*np.exp(amp_exponent)
+
+def make_grids(rho, phi):
+    """Convert rho and phi into meshgrids."""
+    if type(rho) is float:
+        rho = np.array([rho])
+    if type(phi) is float:
+        phi = np.array([phi])
+
+    if len(rho.shape) == 1 and len(phi.shape) == 1:
+        rho, phi = np.meshgrid(rho, phi)
+
+    return rho, phi
+
+def norm(w, p=0, l=0):
+    """ Calculate the normalization constant for the p, l G-L mode with beam width w.
+
+    Arguments:
+        w:  beam radius - float.
+        p:  axial mode index - integer.
+        l:  azimuthal mode index - integer.
+
+    Returns:
+        normalization constant - float.
+        """
+    return np.sqrt(2/(np.pi) * np.factorial(p)/np.factorial(p_m)) / w
+
 
 def Lpl(x, p=0, l=0):
-    """return the value of the Gaussian-Laguerre polynomial at x"""
+    """return the value of the Gaussian-Laguerre polynomial at x
+
+    Arguments:
+        x: value to evaluate function at - float or numpy array
+        p:   axial mode index
+        l:   azimuthal mode index
+
+    Returns:
+        float or numpy array (according to type of rho) containing the function value."""
     return sp.special.eval_genlaguerre(p, abs(l), x)
 
-def Cpl(p=0, l=0):
-    """return the normalization factor for the p,lth G-L mode"""
-    return np.sqrt(4*np.pi)*np.sqrt((2*factorial(p)/(np.pi*factorial(abs(l)+p))))
+def Sr(rho, w, p=0, l=0):
+    """Calculate the radial function of the p, l, G-L mode.
 
-def sign(theta):
-    """return the sign to use in the far field exponent.
-    +1 for theta between 0 and 90 deg
-    -1 for theta between 90 and 180 deg"""
-    return np.sign(np.pi/2 - theta)
+    Arguments:
+        rho: radial distance - numpy array or float
+        w:   beam radius - float
+        p:   axial mode index
+        l:   azimuthal mode index
 
-def exponent(rho, phi, z, w0, kk, p=0, l=0):
-    """return the exponent of e in Glm mode"""
-    bb = b(w0, kk)
-    ww = w(z, w0, bb)
-    FF = F(rho, z, bb)
-    aa = alpha(rho, ww, FF)
-    RR = R(z, bb)
-    PP = Phi0(z, bb)
+    Returns:
+        float or numpy array (according to type of rho) containing the radial
+        distribution."""
+    return (np.sqrt(2*rho)/w)**m * Lpl(2*rho**2 / w**2, p, l)
 
-    return -(aa**2)/2 -j*kk*RR*(FF-1) -j*kk*z +j*(2*p + abs(l) + 1)*PP +j*l*phi
+def phi0_from_w0z(w0, z ,lm):
+    """Calculate the beam phase slippage from beam waist radius, axial distance,
+    and wavelength.
 
-def exponent_ff(theta, phi, a, p=0, l=0):
-    """return the farfield exponent of e in Glm_ff mode"""
-    theta_d = theta+np.pi*1e-16
-    exp = -(a**2)/2 +sign(theta_d)*j*(2*p + abs(l) + 1)*np.pi/2 +j*l*phi
-    return exp
+    Arguments:
+        w0: beam waist radius - float.
+        z: axial distance along beam - float.
+        lm: wavelength - float.
+        All arguments assumed to be in the same units.
 
-def Glm(rho, phi, z, kk, w0, p=0, l=0):
-    """return the value of the modified G-L mode"""
-    bb = b(w0, kk)
-    ww = w(z, w0, bb)
-    FF = F(rho, z, bb)
-    aa = alpha(rho, ww, FF)
-    cosTh = 1.0/FF
+    Returns:
+        phi0: beam phase slippage in radians - float.
+        """
+    phi0 = np.arctan((lm*z)/(np.pi*w0**2))
+    return phi0
 
-    return Cpl(p, l) * (1+cosTh)/2 * 1/(kk*ww*FF) * aa**abs(l) * Lpl(aa**2, p, l) * np.exp(exponent(rho, phi, z, kk, w0, p, l))
+# The following functions allow conversions from pairs of Gaussian beam parameters
+# (w0, w, R, z) to other Gaussian beam parameters
 
-def Glm_ff(theta, phi, kk, w0, p=0, l=0):
-    """return the Farfield value of the modified G-L mode"""
-    aa = alpha_ff(theta, kk, w0)
-    return Cpl(p, l) * (1+np.abs(np.cos(theta)))/4 * kk*w0 * aa**abs(l) * Lpl(aa**2, p, l) * np.exp(exponent_ff(theta, phi, aa, p, l))
+def w_from_w0z(w0, z, lm):
+    """Calculate the beam radius at z from beam waist radius, z,
+    and wavelength.
+
+    Arguments:
+        w0: beam waist radius - float.
+        z: axial distance along beam - float.
+        lm: wavelength - float.
+        All arguments assumed to be in the same units.
+
+    Returns:
+        w: beam radius at z in same units as input - float.
+    """
+    w = w0*np.sqrt(1 + ((lm*z)/(np.pi*w0**2))**2)
+    return w
+
+def R_from_w0z(w0, z, lm):
+    """Calculate the radius of phase curvature at z from beam waist radius, z,
+    and wavelength.
+
+    Arguments:
+        w0: beam waist radius - float.
+        z: axial distance along beam - float.
+        lm: wavelength - float.
+        All arguments assumed to be in the same units.
+
+    Returns:
+        R: radius of phase curvature at z in same units as input - float.
+    """
+    R = z*(1 + ((np.pi*w0**2)/(lm*z))**2)
+    return R
+
+def w0_from_zR(z, R, lm):
+    """Calculate the beam waist radius from distance along beam, radius of phase curvature
+    and wavelength.
+
+    Arguments:
+        z: axial distance along beam - float.
+        R: radius of phase curvature - float.
+        lm: wavelength - float.
+        All arguments assumed to be in the same units.
+
+    Returns:
+        w0: beam waist radius in same units as input - float.
+    """
+    w0 = np.sqrt(lm/np.pi * np.sqrt(z*(R-z)))
+    return w0
+
+def w0_from_zw(z, w, lm):
+    """Calculate the beam waist radius from distance along beam, beam radius
+    and wavelength.
+
+    Arguments:
+        z: axial distance along beam - float.
+        w: beam radius at z - float.
+        lm: wavelength - float.
+        All arguments assumed to be in the same units.
+
+    Returns:
+        w0: beam waist radius in same units as input - float.
+    """
+    w02 = w**2/2 * (1 + np.sqrt(1-(2*lm*z/(np.pi*w**2))**2))
+    if w02 < 0:
+        w02 = w**2/2 * (1 - np.sqrt(1-(2*lm*z/(np.pi*w**2))**2))
+
+    return np.sqrt(w0)
+
+def z_from_w0w(w0, w, lm):
+    """Calculate the distance along beam from beam waist radius, beam radius
+    and wavelength.
+
+    Arguments:
+        w0: beam waist radius - float.
+        w: beam radius at z - float.
+        lm: wavelength - float.
+        All arguments assumed to be in the same units.
+
+    Returns:
+        z: axial distance along beam in same units as input - float.
+    """
+    z = np.pi*w0/lm * np.sqrt(w**2-w0**2)
+    return z
+
+def z_from_w0R(w0, R, lm):
+    """Calculate the distance along beam from beam waist radius, radius of phase curvature
+    and wavelength.
+
+    Arguments:
+        w0: beam waist radius - float.
+        R: radius of phase curvature at z - float.
+        lm: wavelength - float.
+        All arguments assumed to be in the same units.
+
+    Returns:
+        z: axial distance along beam in same units as input - float.
+    """
+    z = R/2 * (1 + np.sqrt(1 - (2*np.pi*w0**2/(lm*R))**2))
+    return z
+
+def w0_from_wR(w, R, lm):
+    """Calculate the beam waist radius from beam radius, radius of phase curvature
+    and wavelength.
+
+    Arguments:
+        w: beam radius - float.
+        R: radius of phase curvature - float.
+        lm: wavelength - float.
+        All arguments assumed to be in the same units.
+
+    Returns:
+        w0: beam waist radius in same units as input - float.
+        """
+        w0 = w / np.sqrt(1 + ((np.pi*w**2)/(lm*R))**2)
+        return w0
+
+def z_from_wR(w, R, lm):
+    """Calculate the axial distance from beam radius, radius of phase curvature
+    and wavelength.
+
+    Arguments:
+        w: beam radius - float.
+        R: radius of phase curvature - float.
+        lm: wavelength - float.
+        All arguments assumed to be in the same units.
+
+    Returns:
+        z: axial distance along beam in units of inputs - float."""
+    z = R / (1 + ((lm*R)/(np.pi*w**2))**2)
+    return z
